190f14f39673c2bfbed657514aef9d8b61ae3207
hari vasudev <hari_vasudev@yahoo.com>
2020-08-06 13:57:19 +0530

    final commit

---
+++ React Mindmapping.mm
@@ -0,0 +1,84 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Product" FOLDED="false" ID="ID_518538628" CREATED="1596457145618" MODIFIED="1596471045171"><hook NAME="MapStyle">
+    <properties fit_to_viewport="false" edgeColorConfiguration="#808080ff,#000000ff,#ff0033ff,#009933ff,#3333ffff,#ff6600ff,#cc00ccff,#ffbf00ff,#00ff99ff,#0099ffff,#996600ff,#000000ff,#cc0066ff,#33ff00ff,#ff9999ff,#0000ccff,#cccc00ff,#0099ccff,#006600ff,#ff00ccff,#00cc00ff,#0066ccff,#00ffffff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt" TEXT_SHORTENED="true">
+<font SIZE="24"/>
+<richcontent TYPE="DETAILS" LOCALIZED_HTML="styles_background_html"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="bubble" SHAPE_VERTICAL_MARGIN="0.0 pt" TEXT_ALIGN="CENTER" MAX_WIDTH="120.0 pt" MIN_WIDTH="120.0 pt">
+<font NAME="Arial" SIZE="9" BOLD="true" ITALIC="false"/>
+<edge STYLE="bezier" WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details">
+<font SIZE="11" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes" COLOR="#000000" BACKGROUND_COLOR="#ffffff">
+<font SIZE="9" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT">
+<font BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+<edge COLOR="#0000cc"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" UNIFORM_SHAPE="true" MAX_WIDTH="120.0 pt" MIN_WIDTH="120.0 pt">
+<font SIZE="24" ITALIC="true"/>
+<edge STYLE="bezier" WIDTH="3"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font SIZE="24"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="0" RULE="FOR_COLUMNS"/>
+<node TEXT="Components" POSITION="right" ID="ID_1106464274" CREATED="1596457257554" MODIFIED="1596457278631">
+<node TEXT="Root Component" ID="ID_384614881" CREATED="1596457284523" MODIFIED="1596457301034">
+<node TEXT="Header" ID="ID_1898657251" CREATED="1596457341853" MODIFIED="1596457356153">
+<node TEXT="Logo" ID="ID_1184596665" CREATED="1596457482180" MODIFIED="1596457490477"/>
+<node TEXT="navbar" ID="ID_848333105" CREATED="1596457491892" MODIFIED="1596457501077">
+<node TEXT="login component" ID="ID_1441599571" CREATED="1596457897852" MODIFIED="1596457910085"/>
+<node TEXT="signup component" ID="ID_987948844" CREATED="1596457927819" MODIFIED="1596457941730"/>
+<node TEXT="dashboard component" ID="ID_908745707" CREATED="1596458387734" MODIFIED="1596458407303"/>
+</node>
+</node>
+<node TEXT="Footer" ID="ID_345558108" CREATED="1596457346168" MODIFIED="1596457422387">
+<node TEXT="logout component" ID="ID_1535151566" CREATED="1596457949677" MODIFIED="1596457961241"/>
+</node>
+<node TEXT="content" ID="ID_914586016" CREATED="1596457555275" MODIFIED="1596457583960">
+<node TEXT="all product component" ID="ID_384775394" CREATED="1596457585376" MODIFIED="1596471058382">
+<node TEXT="product component" ID="ID_902092914" CREATED="1596457600971" MODIFIED="1596457616323">
+<node TEXT="product details" ID="ID_1274501445" CREATED="1596457661951" MODIFIED="1596702313359">
+<node TEXT="product title" ID="ID_560450496" CREATED="1596457714172" MODIFIED="1596457723055"/>
+<node TEXT="product description" ID="ID_601042929" CREATED="1596457725832" MODIFIED="1596457750034"/>
+<node TEXT="product price" ID="ID_1955479339" CREATED="1596457751724" MODIFIED="1596457766334"/>
+<node TEXT="product rating" ID="ID_473947761" CREATED="1596457767755" MODIFIED="1596457777912"/>
+<node TEXT="product stock" ID="ID_188539203" CREATED="1596702333827" MODIFIED="1596702344137"/>
+</node>
+<node TEXT="product image" ID="ID_852609019" CREATED="1596457672852" MODIFIED="1596702319452"/>
+</node>
+<node TEXT="product component" ID="ID_727212514" CREATED="1596470967095" MODIFIED="1596470993220"/>
+<node TEXT="search component" ID="ID_323856045" CREATED="1596457617580" MODIFIED="1596470999926"/>
+</node>
+<node TEXT="Home component" ID="ID_1461160079" CREATED="1596470943773" MODIFIED="1596470956218">
+<node TEXT="search component" ID="ID_970180516" CREATED="1596471007823" MODIFIED="1596471018083"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
